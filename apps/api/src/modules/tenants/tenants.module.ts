b99d72a30261daef2b0ae2645e078e219a91f7c4
friscapuff <a.masri96@gmail.com>
2026-05-17 00:17:28 +0300

    feat: register OrganizationController in TenantsModule

--- apps/api/src/modules/tenants/tenants.module.ts
+++ apps/api/src/modules/tenants/tenants.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Tenant } from '@aierp/database';
 import { TenantsController } from './tenants.controller';
 import { TenantsService } from './tenants.service';
+import { OrganizationController } from './organization.controller';
 
 @Module({
-  controllers: [TenantsController],
+  imports: [TypeOrmModule.forFeature([Tenant])],
+  controllers: [TenantsController, OrganizationController],
   providers: [TenantsService],
+  exports: [TenantsService],
 })
 export class TenantsModule {}
